3774b83b0ab93c3cb27532e3f09e686daa142d33
duttondj <duttondj@vt.edu>
2016-01-12 22:23:02 -0500

    Added multibyte write support

--- software/pigletctl.py
+++ software/pigletctl.py
@@ -25,21 +25,28 @@ address = 0x08
 specified in the Teensy code and can be changed but both must match.
 """
 
+data_reg = 0x00
+"""data_reg: This value is not really used because we are not sending
+the data to any specific register so 0x00 will do.
+"""
+
 def sendKey(value):
 	"""
 		sendKey uses the module-level variables to send the value
-		param through the bus to the address. This returns any error
-		codes generated from write_byte. The docs really suck for this
-		so who knows what it will return.
+		param through the bus to the address. It first converts the
+		string into a list of ascii codes then sends. This returns any
+		error codes generated from write_byte. The docs really suck
+		for this so who knows what it will return.
 	"""
-	return bus.write_byte(address, value)
+	ascii = [ord(ch for word in value for ch in word)]
+	return bus.write_i2c_block_byte(address, data_reg, value)
 	
 # Ask for input and check if not null
 while True:
-	var = input("Keypress to send: ")
-	if var:
-		continue
+	char = input("Keypresses to send (32 char limit): ")
+	if (char and (len(char) <= 32)):
+		break
 
-# Output var and any error code from I2C write
-print("Sending ", var, " via USB keyboard\n")
-print("Error code: ", sendKey(var))
+# Output char and any error code from I2C write
+print("Sending ", char, " via USB keyboard\n")
+print("Error code: ", sendKey(char))
