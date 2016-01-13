2e7195752b8c9480424cd131081ade2e263e8e6b
duttondj <duttondj@vt.edu>
2016-01-12 21:20:55 -0500

    Added comments to pigletctl

--- software/pigletctl.py
+++ software/pigletctl.py
@@ -1,18 +1,45 @@
+#!/usr/bin/env python
+
+__author__ = "Danny Dutton"
+__credits__ = []
+__maintainer__ = "Danny Dutton"
+
+__license__ = "MIT"
+__email__ = "duttondj@vt.edu"
+__status__ = "Prototype"
+
+# Built-in modules:
+
+
+# Third-party modules:
 import smbus
-import time
+
 
 bus = smbus.SMBus(1)
+"""bus: The I2C bus we are using. In the Raspberry Pi B+, this is 1.
+In other versions, this may be a 0. Read your documentation.
+"""
 
 address = 0x08
+"""address: This is a hardcoded I2C address of the Teensy. This is 
+specified in the Teensy code and can be changed but both must match.
+"""
 
 def sendKey(value):
-	bus.write_byte(address, value)
-	return -1
+	"""
+		sendKey uses the module-level variables to send the value
+		param through the bus to the address. This returns any error
+		codes generated from write_byte. The docs really suck for this
+		so who knows what it will return.
+	"""
+	return bus.write_byte(address, value)
 	
+# Ask for input and check if not null
 while True:
 	var = input("Keypress to send: ")
 	if not var:
 		continue
 
-sendKey(var)
+# Output var and any error code from I2C write
 print("Sending ", var, " via USB keyboard\n")
+print("Error code: ", sendKey(var))
